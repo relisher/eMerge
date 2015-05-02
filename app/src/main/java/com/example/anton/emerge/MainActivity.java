f1434dba5754cb55d813668f7ab78afd38f173aa
relisher <anton.relin@gmail.com>
2015-05-02 14:38:05 -0400

    fucking button

--- app/src/main/java/com/example/anton/emerge/MainActivity.java
+++ app/src/main/java/com/example/anton/emerge/MainActivity.java
@@ -1,40 +1,72 @@
 package com.example.anton.emerge;
 
+
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private TextView mainTextView;
+    private static final int request_code = 5;
+    public CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         FacebookSdk.sdkInitialize(getApplicationContext());
+        mCallbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_main);
+        setTitle("LoginExample Cool App");
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        loginButton.setReadPermissions("email");
+        LoginManager.getInstance().logOut(); //ensures that whenever we start the app we're logged out
+
+        LoginManager.getInstance().registerCallback(mCallbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        Profile curProfile = Profile.getCurrentProfile();
+                        startNewIntent(curProfile);
+                    }
+                    @Override
+                    public void onCancel() {
+                        // App code
+                        mainTextView.setText("Status: Logging in cancelled");
+                    }
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                        mainTextView.setText("Status: EXCEPTION: " + exception.toString());
+                    }
+                });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    private void startNewIntent(Profile elUsero){
+        Intent i = new Intent(this, MainMenuTest.class);
 
-        // Logs 'install' and 'app activate' App Events.
-        AppEventsLogger.activateApp(this);
+        i.putExtra("firstName", elUsero.getFirstName());
+        i.putExtra("lastName", elUsero.getLastName());
+        i.putExtra("picURI", elUsero.getProfilePictureUri(1500,1500).toString());
+        startActivityForResult(i, request_code);
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        // Logs 'app deactivate' App Event.
-        AppEventsLogger.deactivateApp(this);
-    }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -56,5 +88,29 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        if ((requestCode == request_code) && (resultCode == RESULT_OK)) {
+            LoginManager.getInstance().logOut();
+            mainTextView.setText("Status: Logged Out!");
+        }
     }
 }
---
+++ app/src/main/java/com/example/anton/emerge/MainMenuTest.java
@@ -0,0 +1,7 @@
+package com.example.anton.emerge;
+
+/**
+ * Created by anton on 5/2/2015.
+ */
+public class MainMenuTest {
+}
