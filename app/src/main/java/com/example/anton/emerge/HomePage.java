c3faea728a395315c51c4f7a8137259095dded2c
Josh <jhellerstein96@gmail.com>
2015-05-02 18:24:16 -0400

    pre-kairos integration

---
+++ app/src/main/java/com/example/anton/emerge/HomePage.java
@@ -0,0 +1,75 @@
+package com.example.anton.emerge;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.facebook.appevents.AppEventsLogger;
+
+/**
+ * Created by Josh on 5/2/2015.
+ */
+public class HomePage extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+
+    public void addFriend(View view){
+        Intent i = new Intent(this, Camera.class);
+        startActivity(i);
+    }
+
+    public void pictureInfo(View view){
+        Intent i = new Intent(this, PictureInfo.class);
+        startActivity(i);
+    }
+
+    public void pictureViewActivity(View view){
+        Intent i = new Intent(this, PictureViewActivity.class);
+        startActivity(i);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+}
