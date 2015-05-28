3dae5ab06016f0e8df851f9b87e1ce1c162a54c2
JoeAzar <jazar@acm.org>
2015-05-28 18:41:12 -0400

    Threading bullshit/ gallery (Josh)

--- app/src/main/java/com/example/anton/emerge/Gallery.java
+++ app/src/main/java/com/example/anton/emerge/Gallery.java
@@ -1,24 +1,202 @@
 package com.example.anton.emerge;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
-import android.view.Gravity;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import com.kairos.KairosListener;
-
-import org.json.JSONException;
+import android.widget.EditText;
 
 import java.io.IOException;
 
 public class Gallery extends Activity {
 
+    public static final String PACKAGE_NAME = "com.example.anton.emerge";
+
+    public static final String DATA_PATH = Environment
+            .getExternalStorageDirectory().toString() + "/eMerge/";
+
+    //gallery load variables
+    private static int RESULT_LOAD_IMAGE = 1;
+    private static final int PICK_FROM_GALLERY = 2;
+    Bitmap thumbnail = null;
+    String picturePath;
+    Bitmap bitmap;
+
+    public static final String lang = "eng";
+    private static final String TAG = "Gallery.java";
+
+    // protected ImageView _image;
+    protected EditText _field;
+    protected String _path;
+    protected boolean _taken;
+    protected static final String PHOTO_TAKEN = "photo_taken";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startGalleryActivity();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    // Simple android photo capture:
+    // http://labs.makemachine.net/2010/03/simple-android-photo-capture/
+
+    protected void startGalleryActivity() {
+
+        Intent in = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(in, RESULT_LOAD_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data){
+
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            //raw image
+            thumbnail = (BitmapFactory.decodeFile(picturePath));
+
+        }
+
+        LoadData task = new LoadData();
+        task.execute();
+    }
+
+    protected void onPhotoPicked() {
+        Log.e(TAG, "PHOTO WAS PICKED");
+        _taken = true;
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+
+        bitmap = BitmapFactory.decodeFile(picturePath, options);
+
+        try {
+            ExifInterface exif = new ExifInterface(picturePath);
+            int exifOrientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+
+            Log.v(TAG, "Orient: " + exifOrientation);
+
+            int rotate = 0;
+
+            switch (exifOrientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate = 270;
+                    break;
+            }
+
+            Log.v(TAG, "Rotation: " + rotate);
+
+            if (rotate != 0) {
+
+                // Getting width & height of the given image.
+                int w = bitmap.getWidth();
+                int h = bitmap.getHeight();
+
+                // Setting pre rotate
+                Matrix mtx = new Matrix();
+                mtx.preRotate(rotate);
+
+                // Rotating Bitmap
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+            }
+
+            // Convert to ARGB_8888, required by tess
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+        } catch (IOException e) {
+            Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+        }
+
+        Intent i = new Intent(this, SearchActivity.class);
+        i.putExtra("BitmapImage", bitmap);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
+    }
+
+
+    public class LoadData extends AsyncTask<Void, Void, Void> {
+        ProgressDialog progressDialog;
+        //declare other objects as per your need
+        @Override
+        protected void onPreExecute()
+        {
+            progressDialog= ProgressDialog.show(Gallery.this, "Processing","Processing please wait...", true);
+
+            //do initialization of required objects objects here
+        };
+        @Override
+        protected Void doInBackground(Void... params)
+        {
+            onPhotoPicked();
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+        };
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
 
     // this is the action code we use in our intent,
     // this way we know we're looking at the response from our own action
@@ -99,7 +277,7 @@ public class Gallery extends Activity {
             }
         }
     }
-}
+*/
 
 
 
--- app/src/main/java/com/example/anton/emerge/SearchActivity.java
+++ app/src/main/java/com/example/anton/emerge/SearchActivity.java
@@ -1,17 +1,20 @@
 package com.example.anton.emerge;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.kairos.KairosListener;
 
@@ -25,79 +28,14 @@ import java.io.UnsupportedEncodingException;
  */
 public class SearchActivity extends Activity {
 
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            //setContentView(R.layout.activity_home_page);
 
-            // Create an instance of the KairosListener
-            KairosListener listener = new KairosListener() {
-
-                @Override
-                public void onSuccess(String response) {
-                    Toast.makeText(getApplicationContext(), "Loading...",
-                            Toast.LENGTH_LONG).show();
-                    Log.d("Joe",response);
-                    try {
-                        JSONObject obj = new JSONObject(response);
-
-                        String fbId = obj.getJSONArray("images").getJSONObject(0).getJSONObject("transaction").getString("subject");
-                        Log.d("Joe", fbId);
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://facebook.com/"+fbId));
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        intent.setPackage("com.android.chrome");
-                        try {
-                            startActivity(intent);
-                        } catch (ActivityNotFoundException ex) {
-                            // Chrome browser presumably not installed so allow user to choose instead
-                            intent.setPackage(null);
-                            startActivity(intent);
-                        }
-                    }catch(JSONException e){
-                        Log.d("Joe",e.toString());
-                        Toast.makeText(getApplicationContext(), "No match found, please try again",
-                                Toast.LENGTH_LONG).show();
-                    }
-
-
-                }
-
-                @Override
-                public void onFail(String response) {
-                    // your code here!
-                    Log.d("KAIROS DEMO", response);
-                }
-            };
-
-
-            try {
-                // Fine-grained Example:
-                // This example uses a bitmap image and also optional parameters
-                Intent intent = getIntent();
-                Bitmap image = (Bitmap) intent.getParcelableExtra("BitmapImage");
-                String galleryId = "friends1";
-                String selector = "FULL";
-                String threshold = "0.70";
-                String minHeadScale = "0.25";
-                String maxNumResults = "25";
-                MainActivity.myKairos.recognize(image,
-                        galleryId,
-                        selector,
-                        threshold,
-                        minHeadScale,
-                        maxNumResults,
-                        listener);
-
-            } catch (JSONException e1) {
-            } catch (UnsupportedEncodingException e) {
-            }
-
-            Intent homeIntent = new Intent(this, HomePage.class);
-            homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(homeIntent);
-            //homeIntent.putExtra("picURI", "x");
-            //startActivityForResult(homeIntent, 6);
-        }
+            LoadData task = new LoadData();
+            task.execute();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -142,4 +80,110 @@ public class SearchActivity extends Activity {
     }
 
 
+    public class LoadData extends AsyncTask<Void, Void, Void> {
+        ProgressDialog progressDialog;
+        //declare other objects as per your need
+        @Override
+        protected void onPreExecute()
+        {
+            progressDialog= ProgressDialog.show(SearchActivity.this, "Processing","Processing please wait...", true);
+
+            //do initialization of required objects objects here
+        }
+        @Override
+        protected Void doInBackground(Void... params)
+        {
+            Handler mainHandler = new Handler(Looper.getMainLooper());
+
+            Runnable myRunnable = new R_KairosLauncher(); // This is your code
+            boolean b = mainHandler.post(myRunnable);
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+        }
+    }
+class R_KairosLauncher implements Runnable {
+    public void launchKairos() {
+
+        // Create an instance of the KairosListener
+        KairosListener listener = new KairosListener() {
+
+            @Override
+            public void onSuccess(String response) {
+                Log.v("Joe", "Loading...");
+                Log.d("Joe", response);
+                try {
+                    JSONObject obj = new JSONObject(response);
+
+                    String fbId = obj.getJSONArray("images").getJSONObject(0).getJSONObject("transaction").getString("subject");
+                    Log.d("Joe", fbId);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://facebook.com/" + fbId));
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setPackage("com.android.chrome");
+                    try {
+                        startActivity(intent);
+                    } catch (ActivityNotFoundException ex) {
+                        // Chrome browser presumably not installed so allow user to choose instead
+                        intent.setPackage(null);
+                        startActivity(intent);
+                    }
+                } catch (JSONException e) {
+                    Log.d("Joe", e.toString());
+                    Log.e("Joe", "No match found, please try again");
+                }
+
+
+            }
+
+            @Override
+            public void onFail(String response) {
+                // your code here!
+                Log.d("KAIROS DEMO", response);
+            }
+        };
+
+
+        try {
+            // Fine-grained Example:
+            // This example uses a bitmap image and also optional parameters
+            Intent intent = getIntent();
+            Bitmap image = (Bitmap) intent.getParcelableExtra("BitmapImage");
+            String galleryId = "friends1";
+            String selector = "FULL";
+            String threshold = "0.70";
+            String minHeadScale = "0.25";
+            String maxNumResults = "25";
+            MainActivity.myKairos.recognize(image,
+                    galleryId,
+                    selector,
+                    threshold,
+                    minHeadScale,
+                    maxNumResults,
+                    listener);
+
+        } catch (JSONException e1) {
+        } catch (UnsupportedEncodingException e) {
+        }
+
+        Intent homeIntent = new Intent(SearchActivity.this, HomePage.class);
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(homeIntent);
+
+        //homeIntent.putExtra("picURI", "x");
+        //startActivityForResult(homeIntent, 6);
+    }
+
+    @Override
+    public void run() {Log.e("Joe", "HELP I'M TRAPPED IN A THREAD FACTORY");
+        launchKairos();
+    }
 }
+
+
+}
+
